d507671a93f384d9636b04e1592247139b5f8b63
kinyeramos2021 <kinyeramo@gmail.com>
2021-10-03 17:51:39 +0300

    tsc

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import express from 'express';
-
-
-const app = express();
-
-const port = process.env.PORT || 4005;
-let runningMessage = 'Server is running on' + port;
-
-app.get('/', (req, res) => {
-  console.log('API was successfully requested');
-  res.send(runningMessage);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+var port = process.env.npm_package_config_port || 4005;
+var runningMessage = 'Server is running on port ' + port;
+app.get('/', function (req, res) {
+    console.log('API was successfully requested');
+    res.send(runningMessage + 'hello!');
 });
-
-const server = app.listen(port, () => {
-  console.log(runningMessage);
+var server = app.listen(port, function () {
+    console.log(runningMessage);
 });
-
 module.exports = server;
